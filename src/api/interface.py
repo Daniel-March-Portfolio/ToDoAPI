9e4a807aa0e6a70f70a54ffe80b90bde088c4c81
Daniel-March <town.kor@gmail.com>
2023-11-29 18:12:09 +0300

    Updated API interface

--- src/api/interface.py
+++ src/api/interface.py
@@ -1,12 +1,36 @@
+import typing
 from abc import ABC, abstractmethod
 
+from sqlalchemy.ext.asyncio import AsyncEngine
+
 from src.api.state import State
+from src.core.redis import RedisInterface
+from src.env import EnvInterface
+
+if typing.TYPE_CHECKING:
+    from src.api.router.interface import RouterInterface
 
 
 class APIInterface(ABC):
     @property
     @abstractmethod
     def state(self) -> State: ...
+
+    @property
+    @abstractmethod
+    def redis(self) -> RedisInterface: ...
+
+    @property
+    @abstractmethod
+    def env(self) -> EnvInterface: ...
+
+    @property
+    @abstractmethod
+    def database_engine(self) -> AsyncEngine: ...
+
+    @property
+    @abstractmethod
+    def router(self) -> "RouterInterface": ...
 
     @abstractmethod
     def run(self) -> None: ...
