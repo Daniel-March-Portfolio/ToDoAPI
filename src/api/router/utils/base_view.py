0c0d05803828ae04e5c39339cda9c16f0bd2494e
Daniel-March <town.kor@gmail.com>
2023-11-29 18:08:11 +0300

    Added link to API in BaseView

--- src/api/router/utils/base_view.py
+++ src/api/router/utils/base_view.py
@@ -1,5 +1,16 @@
-from aiohttp.web import View
+from aiohttp.web import View, Request as AiohttpRequest
+
+from src.api.interface import APIInterface
+
+
+class Request(AiohttpRequest):
+
+    @property
+    def api(self) -> APIInterface:
+        return super().app()
 
 
 class BaseView(View):
-    pass
+    @property
+    def request(self) -> Request:
+        return super().request
