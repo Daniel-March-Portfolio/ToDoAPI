d33bc8ec269d2d94d20137185c9cde166b4134e5
Daniel-March <town.kor@gmail.com>
2023-11-29 18:00:25 +0300

    Added 'api_session_ttl' and 'api_salt' fields to EnvInterface

--- src/env/interface.py
+++ src/env/interface.py
@@ -13,3 +13,12 @@ class EnvInterface(ABC):
     @property
     @abstractmethod
     def api_port(self) -> int: ...
+
+    @property
+    @abstractmethod
+    def api_session_ttl(self) -> int:
+        """Session TTL in seconds"""
+
+    @property
+    @abstractmethod
+    def api_salt(self) -> str: ...
