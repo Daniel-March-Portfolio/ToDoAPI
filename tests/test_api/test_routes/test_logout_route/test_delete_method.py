c5aa11dc1b4e979155e937477fc41d467b2e4b9b
Daniel-March <town.kor@gmail.com>
2023-11-29 22:47:58 +0300

    Wrote tests for /logout DELETE method

---
+++ tests/test_api/test_routes/test_logout_route/test_delete_method.py
@@ -0,0 +1,56 @@
+import pytest
+from aiohttp.web_response import Response
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from src.api import APIInterface
+from src.api.router.logout.methods import Delete
+from tests.data_classes import UserDataClass
+from tests.test_api.data_classes import RequestDataClass
+
+
+class Request(RequestDataClass):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_method(api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass]):
+    await api.redis.set("some_session", "")
+
+    method = Delete(
+        request=Request(
+            api=api,
+            cookies={"session": "some_session"}
+        ),
+    )
+
+    session_in_redis = await api.redis.get("some_session")
+    assert session_in_redis is not None
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is True, method.error
+
+    handle_successful = await method.handle()
+    assert handle_successful is True, method.error
+
+    prepare_response_successful = await method.prepare_response()
+    assert prepare_response_successful is True, method.error
+
+    assert isinstance(method.response, Response), method.response
+    assert method.response.body is None
+    assert method.response.status == 204
+
+    session_in_redis = await api.redis.get("some_session")
+    assert session_in_redis is None
+
+
+@pytest.mark.asyncio
+async def test_for_empty_cookies(api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass]):
+    method = Delete(
+        request=Request(
+            api=api,
+        ),
+    )
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is False
+    assert method.error == {"status": 400, "errors": ["session not found in cookies"]}
