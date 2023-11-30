5f57af897343dbef772724af3ad48a46ea73f36f
Daniel-March <town.kor@gmail.com>
2023-11-30 16:30:36 +0300

    Wrote tests for /tasks DELETE method

---
+++ tests/test_api/test_routes/test_tasks_route/test_tasks_route_delete_method.py
@@ -0,0 +1,143 @@
+from typing import Any
+from uuid import uuid4
+
+import pytest
+from aiohttp.web_response import Response
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from src.api import APIInterface
+from src.api.router.tasks.methods import Delete
+from src.core.models import User, Task
+from tests.data_classes import UserDataClass, TaskDataClass
+from tests.test_api.data_classes import RequestDataClass
+
+
+class Request(RequestDataClass):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_method(
+        api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass], normal_tasks: list[TaskDataClass]
+):
+    user = User(
+        login=normal_users[0].login,
+        name=normal_users[0].name,
+        password_hash=normal_users[0].password_hash
+    )
+    await user.save(engine)
+    await Task(
+        title=normal_tasks[0].title,
+        user_uuid=user.uuid
+    ).save(engine)
+    await Task(
+        title=normal_tasks[1].title,
+        user_uuid=user.uuid
+    ).save(engine)
+
+    session = "some_session"
+    await api.redis.set(session, user.uuid.hex)
+
+    n_tasks = await Task.get_count_of_rows(engine)
+    assert n_tasks == 2
+
+    method = Delete(
+        request=Request(
+            api=api,
+            cookies={"session": session}
+        ),
+    )
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is True, method.error
+
+    handle_successful = await method.handle()
+    assert handle_successful is True, method.error
+
+    prepare_response_successful = await method.prepare_response()
+    assert prepare_response_successful is True, method.error
+
+    n_tasks = await Task.get_count_of_rows(engine)
+    assert n_tasks == 0
+
+    assert isinstance(method.response, Response), method.response
+    assert method.response.status == 204
+    assert method.response.body is None
+
+
+@pytest.mark.asyncio
+async def test_for_unauthorized_user(
+        api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass], normal_tasks: list[TaskDataClass]
+):  # ToDo Deside what to do with this test because it is already tested in utils tests
+    user = User(
+        login=normal_users[0].login,
+        name=normal_users[0].name,
+        password_hash=normal_users[0].password_hash
+    )
+    await user.save(engine)
+
+    method = Delete(
+        request=Request(
+            api=api,
+            raw_json={"uuid": uuid4().hex}
+        ),
+    )
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is False
+
+    assert method.error == {"status": 403, "errors": ["bad session"]}
+
+
+@pytest.mark.asyncio
+async def test_for_expired_session(
+        api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass], normal_tasks: list[TaskDataClass]
+):  # ToDo Deside what to do with this test because it is already tested in utils tests
+    user = User(
+        login=normal_users[0].login,
+        name=normal_users[0].name,
+        password_hash=normal_users[0].password_hash
+    )
+    await user.save(engine)
+    session = "some_session"
+
+    method = Delete(
+        request=Request(
+            api=api,
+            raw_json={"uuid": uuid4().hex},
+            cookies={"session": session}
+        ),
+    )
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is False
+
+    assert method.error == {"status": 403, "errors": ["bad session"]}
+
+
+@pytest.mark.asyncio
+async def test_for_deleted_user(
+        api: APIInterface, engine: AsyncEngine, normal_users: list[UserDataClass], normal_tasks: list[TaskDataClass]
+):  # ToDo Deside what to do with this test because it is already tested in utils tests
+    user = User(
+        login=normal_users[0].login,
+        name=normal_users[0].name,
+        password_hash=normal_users[0].password_hash
+    )
+    await user.save(engine)
+    session = "some_session"
+    await api.redis.set(session, user.uuid.hex)
+    await user.delete(engine)
+
+    method = Delete(
+        request=Request(
+            api=api,
+            raw_json={"uuid": uuid4().hex},
+            cookies={"session": session}
+        ),
+    )
+
+    prepare_request_successful = await method.prepare_request()
+    assert prepare_request_successful is False
+
+    assert method.error == {"status": 403, "errors": ["bad session"]}
