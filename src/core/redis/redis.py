0ae056d1cd3910003aecd7d4101ae3ba077085f3
Daniel-March <town.kor@gmail.com>
2023-12-03 14:49:41 +0300

    Updated Redis creation

--- src/core/redis/redis.py
+++ src/core/redis/redis.py
@@ -4,4 +4,7 @@ from src.core.redis.interface import RedisInterface
 
 
 class Redis(_Redis, RedisInterface):
-    pass
+    def __new__(cls, *args, url: str = None, **kwargs) -> "Redis":
+        if url is not None:
+            return cls.from_url(url=url)
+        return _Redis(*args, **kwargs)
